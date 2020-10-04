96fd3f4df9c0dc38ded520dcfefc89c068019a4f
adam lounds <adamlounds@gmail.com>
2020-10-04 17:02:38 +0100

    http store

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import(
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/adamlounds/yoti/client"
 	"github.com/rs/zerolog"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 )
+
+type StoreRequest struct {
+	Id string
+	Payload string
+}
 
 func main() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
@@ -24,6 +33,19 @@ func main() {
 
 	//payload, err := myClient.Retrieve([]byte("def"), aesKey)
 
+	http.HandleFunc("/store", func (w http.ResponseWriter, r * http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			logger.Warn().Msg("cannot read body")
+			http.Error(w, "cannot read body", http.StatusBadRequest)
+			return
+		}
 
+		var req StoreRequest
+		json.Unmarshal(body, &req)
+		aesKey, err := myClient.Store([]byte(req.Id), []byte(req.Payload))
+		fmt.Fprint(w, hex.EncodeToString(aesKey))
+	})
 
+	logger.Info().Err( http.ListenAndServe(":8080", nil))
 }
